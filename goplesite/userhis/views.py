0cc4cf6ebd74e9f090c029ac833f92f464791b07
YanhuiJessica <137126578@qq.com>
2020-06-28 14:55:51 +0800

    models调用方式

--- goplesite/userhis/views.py
+++ goplesite/userhis/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.urls import reverse
 import re
 from .forms import IDForm
-from  .models import Users
-from movie.models import Movies
+from .models import Users, Movies
+
 def index(request):
     form = IDForm()
     return render(request, 'userhis/searchis.html', {'form': form})
@@ -25,6 +25,5 @@ def search(request):
             for movie in watchedMovies:
                  movieId.append(movie.movieId)
             movies=Movies.objects(movieId__in= movieId)
-            
             # return HttpResponseRedirect(reverse('userhis:results', args=(user_id, )))
     return render(request, 'userhis/searchres.html',{ 'user':user_,'movies': zip(movies,watchedMovies)})
