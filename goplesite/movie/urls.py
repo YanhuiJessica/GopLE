b34a99b8a15e62e8ef9913268eb6afb923181c5f
YanhuiJessica <137126578@qq.com>
2020-06-28 11:28:20 +0800

    获取方式修改

--- goplesite/movie/urls.py
+++ goplesite/movie/urls.py
@@ -6,6 +6,7 @@ app_name = 'movie'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
-    path('results/', views.results, name='results'),
-    path('tops/',views.tops,name='tops')
+    path('<str:keyword>/<str:genre>/results/', views.results, name='results'),
+    path('<str:keyword>/results', views.key_results, name='key_results'),
+    path('<str:genre>/results', views.style_results, name='style_results')
 ]
--- goplesite/movie/views.py
+++ goplesite/movie/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -15,17 +15,36 @@ def search(request):
         form = MovieForm(request.POST)
         if form.is_valid():
             keyword = form.cleaned_data['keyword']
-            regk = ".*" + keyword + ".*"
             style = form.cleaned_data['style']
-            regs = ".*" + style + ".*"
-            movies = Movies.objects(title=re.compile(regk, re.IGNORECASE), genres=re.compile(regs, re.IGNORECASE)).order_by('-rating')[:30]
-
-            for movie in movies:
-                print(movie.title)
-                print(movie.rating)
-            return HttpResponseRedirect(reverse('movie:results'))
+            if keyword != '' and style == '':
+                return HttpResponseRedirect(reverse('movie:key_results', args=(keyword, )))
+            elif keyword == '' and style != '':
+                return HttpResponseRedirect(reverse('movie:style_results', args=(style, )))
+            elif keyword != '' and style != '':
+                return HttpResponseRedirect(reverse('movie:results', args=(keyword, style)))
+            else:
+                return HttpResponseRedirect(reverse('movie:index'))
     return render(request, 'movie/movieseares.html', {'form': form})
 
-def results(request):
+def key_results(request, keyword):
     form = MovieForm()
-    return render(request, 'movie/movieseares.html', {'form': form})
+    regk = ".*" + keyword + ".*"
+    movies = Movies.objects(title=re.compile(regk, re.IGNORECASE)).order_by('-rating')[:30]
+    for movie in movies:
+        print(movie.title)
+    return render(request, 'movie/movieseares.html', {'form': form, 'movies': movies})
+
+def style_results(request, style):
+    form = MovieForm()
+    regs = ".*" + style + ".*"
+    movies = Movies.objects(genres=re.compile(regs, re.IGNORECASE)).order_by('-rating')[:30]
+    for movie in movies:
+        print(movie.title)
+    return render(request, 'movie/movieseares.html', {'form': form, 'movies': movies})
+
+def results(request, keyword, style):
+    form = MovieForm()
+    regk = ".*" + keyword + ".*"
+    regs = ".*" + style + ".*"
+    movies = Movies.objects(title=re.compile(regk, re.IGNORECASE), genres=re.compile(regs, re.IGNORECASE)).order_by('-rating')[:30]
+    return render(request, 'movie/movieseares.html', {'form': form, 'movies': movies})
